89ee45ffbbfed685b97a8d74ff3b04b5314e9433
Fabio Claro <fabioclaroj@gmail.com>
2025-06-15 18:15:30 -0300

    Curso de Cypres do zero a nuvem

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,125 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html')
+  });
+
+  it('Verificar o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+
+  it('Preenche os campos obrigatórios e envia o formulário', () => {
+
+    cy.get('#firstName').type('Binho')
+    cy.get('#lastName').type('Claro')
+    cy.get('#email').type('binho@teste.com.br')
+    cy.get('#open-text-area').type('Fazendo validação de teste Fazendo validação de testeFazendo validação de testeFazendo validação de testeFazendo validação de teste', { delay: 0 })
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.success').should('be.visible', 'Mensagem enviada com sucesso.')
+  })
+
+  it('Preenchendo com textos maiores de forma instantânea', () => {
+    const longText = Cypress._.repeat('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 10) //lowdash
+
+    cy.get('#firstName').type('Binho')
+    cy.get('#lastName').type('Claro')
+    cy.get('#email').type('binho@teste.com.br')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.success').should('be.visible', 'Mensagem enviada com sucesso.')
+  })
+
+  it('Exibir mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Binho')
+    cy.get('#lastName').type('Claro')
+    cy.get('#email').type('binho@teste.com,br')
+    cy.get('#open-text-area').type('teste')
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('o campo telefone não deve aceitar valores não-numéricos', () => {
+    cy.get('#phone')
+      .type('abcd')
+      .should('have.value', '')
+  })
+
+  it('exibir mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Binho')
+    cy.get('#lastName').type('Claro')
+    cy.get('#email').type('binho@teste.com,br')
+    cy.get('#open-text-area').type('teste')
+    cy.get('#phone-checkbox').click()
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('Preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName').type('Binho').should('have.value', 'Binho').clear().should('have.value', '')
+    cy.get('#lastName').type('Claro').should('have.value', 'Claro').clear().should('have.value', '')
+    cy.get('#email').type('binho@teste.com,br').should('have.value', 'binho@teste.com,br').clear().should('have.value', '')
+    cy.get('#phone').type('123456789').should('have.value', '123456789').clear().should('have.value', '')
+  })
+
+  it('Exibir mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+  })
+
+  it('Enviar o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible', 'Mensagem enviada com sucesso.')
+  })
+
+  it('Enviar o formuário com sucesso usando um comando customizado e objeto', () => {
+    const data = {
+      firstName: 'fabio',
+      lastName: 'Claro',
+      email: 'binho@teste.com',
+      text: 'Teste.'
+    }
+
+    cy.fillMandatoryFieldsAndSubmit2(data)
+
+    cy.get('.success').should('be.visible', 'Mensagem enviada com sucesso.')
+  })
+
+  it('Enviar o formuário com sucesso usando um comando customizado e preencher defalt', () => {
+    cy.fillMandatoryFieldsAndSubmit3()
+
+    cy.get('.success').should('be.visible', 'Mensagem enviada com sucesso.')
+  })
+
+  it('Selecionar um produto (YouTube) por seu texto', () => {
+    cy.get('#product').select('YouTube').should('have.value', 'youtube')
+  })
+
+  it('Selecionar um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+  })
+
+  it('Selecionar um produto (Blog) por seu índice', () => {
+    cy.get('#product').select(1).should('have.value', 'blog')
+  })
+
+  it('Marcar o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]').check().should('be.checked')
+  })
+
+  it('Validar se não está marcado o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]').should('not.be.checked')
+  })
+
+  it('marca cada tipo de atendimento', () => {
+    cy.get('input[type="radio"]').each(typeOfService => {
+      cy.wrap(typeOfService).check().should('be.checked')
+    })
+  })
+
+})
