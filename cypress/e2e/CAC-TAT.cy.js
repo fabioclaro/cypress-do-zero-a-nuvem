0833fa3182529adc6ff27e5b27c264d692eb86e3
Fabio Claro <fabioclaroj@gmail.com>
2025-07-18 12:33:56 -0300

    Validando status de uma requisição

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -217,5 +217,18 @@ it('preenche o campo da área de texto usando o comando invoke simulando um ctrl
     .invoke('val', 'Um texto qualquer')
     .should('have.value', 'Um texto qualquer')
 })
+it('faz uma requisição HTTP', () => {
+  cy.request('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
+  .as('getRequest')
+  .its('status')
+  .should('be.equal', 200 )
+  cy.get('@getRequest')
+  .its('statusText')
+  .should('be.equal', 'OK')
+  cy.get('@getRequest')
+  .its('body')
+  .should('include', 'CAC TAT')
+})
+
 })
  
